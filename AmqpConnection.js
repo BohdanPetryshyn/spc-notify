2f89d28212e002a9d60f9c2026bffe826f4217d9
bpetryshyn <bpetryshyn@hubspot.com>
2020-03-25 23:55:53 +0200

    Implement AMQP notify services communication

---
+++ AmqpConnection.js
@@ -0,0 +1,55 @@
+const amqp = require('amqplib');
+const EventEmitter = require('events');
+
+const AMQP_URL = process.env.AMQP_HOST || 'amqp://localhost';
+
+class AmqpConnection extends EventEmitter {
+  constructor(exchangeName, queueName) {
+    super();
+    this.exchangeName = exchangeName;
+    this.queueName = queueName;
+
+    this.consumeMessage = this.consumeMessage.bind(this);
+  }
+
+  async init() {
+    this.channel = await this.createChannel();
+    await Promise.all([this.assertExchange(), this.assertQueue()]);
+    await this.bindQueue();
+    this.consumeQueue();
+  }
+
+  async createChannel() {
+    const connection = await amqp.connect(AMQP_URL);
+    return connection.createChannel();
+  }
+
+  assertExchange() {
+    return this.channel.assertExchange(this.exchangeName);
+  }
+
+  assertQueue() {
+    const options = { exclusive: true };
+    return this.channel.assertQueue(this.queueName, options);
+  }
+
+  bindQueue() {
+    return this.channel.bindQueue(this.queueName, this.exchangeName);
+  }
+
+  consumeQueue() {
+    this.channel.consume(this.queueName, this.consumeMessage, { noAck: true });
+  }
+
+  consumeMessage(msg) {
+    const message = JSON.parse(msg.content.toString());
+    this.emit('message', message);
+  }
+
+  publish(message) {
+    const messageBuffer = new Buffer(JSON.stringify(message));
+    this.channel.publish(this.exchangeName, '', messageBuffer);
+  }
+}
+
+module.exports = AmqpConnection;
--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 const PORT = process.env.PORT || 3000;
 const io = require('socket.io')(PORT);
 
+const AmqpConnection = require('./AmqpConnection');
+
+const amqpConnection = new AmqpConnection('spc', `spc-notify-${PORT}`);
+
 const broadcast = message => {
   io.emit('message', message);
 };
 
+amqpConnection.on('message', broadcast);
+
 io.on('connection', socket => {
-  socket.on('message', broadcast);
+  socket.on('message', message => amqpConnection.publish(message));
 });
+
+amqpConnection.init().then(() => console.log('Amqp connected.'));
